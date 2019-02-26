45329caf59be4b3672b3aaf0873adaa501676e6a
Sigeno <>
2019-02-27 00:02:16 +0900

    add hash

--- mydiary/models.py
+++ mydiary/models.py
@@ -3,8 +3,9 @@ from django.utils import timezone
 # Create your models here.
 class Document(models.Model):
     title = models.CharField(max_length=100)
+    hash_key = models.CharField(max_length=100,blank=True)
     content = models.TextField()
     day = models.DateField(default=timezone.now)
 
     def __str__(self):
-        return self.title + ':' + self.content + str(self.day)
+        return self.title + self.hash_key + self.content + str(self.day)
